3754d17a8ac73847a0641df17ec0525330fa6bd0
krisnaRusdiono <38303737+krisnaRusdiono@users.noreply.github.com>
2021-08-12 19:26:36 +0700

    major update 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiodataComponent } from './biodata/biodata.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'biodata', component: BiodataComponent
+  },
+  {
+    path: '', redirectTo: '/biodata', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BiodataComponent } from './biodata/biodata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BiodataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
